9f919969407245dadb5a72198bc82b18a8251222
nishanthi <nishanthi.sietk@gmail.com>
2016-11-14 18:59:50 +0530

    sample text demo commit

---
+++ src/com/test/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String test = "Hello World Program";
+		System.out.println("Hi " + test);
+	}
+
+}
